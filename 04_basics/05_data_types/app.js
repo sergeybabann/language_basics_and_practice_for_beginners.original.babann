8cc4a6a1cdcd47afd913610d42338142b05ca9a0
sergeybabann <sergeybabann@yandex.ru>
2025-10-15 14:25:06 +0300

    05 lesson complete

--- 04_basics/05_data_types/app.js
+++ 04_basics/05_data_types/app.js
@@ -1,1 +1,17 @@
 let a = 5
+let b = 5.6
+console.log(typeof a)
+
+a = 'fdkjvbkjv'
+console.log(typeof a)
+
+let isAdmin = false
+console.log(typeof isAdmin)
+
+let c
+console.log(typeof c)
+c = 5
+console.log(typeof c)
+
+let d = null
+console.log(d == null)
